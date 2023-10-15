b70527ebd3a0721b6a201eea2c031c3288d1975c
dilyorbek777 <asfandiyorovdilyorbek@gmail.com>
2023-10-15 12:16:12 +0500

    ishladi va 100%

--- components/seo/seo.props.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export interface SeoProps {
-    children: ReactNode;
-    metaTitle: string;
-    metaDescription: string;
-    metaKeywords: string;
-    author: string;
-}
